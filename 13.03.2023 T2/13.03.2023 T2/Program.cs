57cb7878efd4440832a033fc8ffb6855ff1d74df
HikmetMehraliyev <7830uh8@code.edu.az>
2023-03-13 06:57:41 +0400

    Exercise

---
+++ 13.03.2023 T2/13.03.2023 T2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//Task 2
+
+int num = 678;
+int i = 0;
+int x = 1;
+
+while (num > 0)
+{
+    i += x * (num % 10);
+        num = (num / 10);
+    x *= 10;
+}
+i = 3 * x + i;
+
+Console.WriteLine(i);
+
---
+++ 13.03.2023/13.03.2023/Program.cs
@@ -0,0 +1,15 @@
+﻿//Task 1
+
+int num = 5987;
+int j = 0;
+int i = 0;
+
+while (num > 0)
+{
+    j = num % 10;
+    i = (i * 10) + j;
+    num = num / 10;
+}
+
+Console.WriteLine(i);
+
